5d90d941da346ec97cdde7cfeabe787be8ff5ca9
hammadhadi0075 <hammadhadi0075@gmail.com>
2025-12-18 20:00:12 +0500

    numpy

--- numpy code.py
+++ numpy code.py
@@ -1,8 +1,13 @@
 import numpy as np
-# 1D array
-arr1 = np.array([1, 2, 3, 4, 5])
+#0d array
+arr0=np.array(5)
+print(arr0)
+#1d array
+arr1=np.array([2,4,6])
 print(arr1)
-
-# 2D array
-arr2 = np.array([[1, 2, 3], [4, 5, 6]])
+#2d array
+arr2=np.array([[2,5,7],[1,8,9]])
 print(arr2)
+#3d array
+arr3=np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(arr3)
